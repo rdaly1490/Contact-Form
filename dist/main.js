356628665dc5330f70087d3be5370f652e30891e
Rob <rdaly1490@gmail.com>
2015-05-26 18:23:58 -0500

    Still need to figure out color fade

--- dist/main.js
+++ dist/main.js
@@ -6,26 +6,54 @@ var $form = $("#form");
 var $btn = $("#btn");
 var $txtArea = $("#text-area");
 var $error = $(".hidden");
-var $inputs = $(".text");
+var $name = $("#name");
+var $email = $("#email");
+var $website = $("#website");
+var $hiddenDiv = $("#submit-div");
 
+$hiddenDiv.hide();
 
 $btn.on("click", submitClicked);
+$btn.on("mouseover", colorIn);
+
+function colorIn(e) {
+	   $btn.animate({backgroundColor: 'green'}, 'slow');
+}
 
 function submitClicked(e) {
 
 	console.log("submitClicked");
-	var name = /[0-9]/;
+	var nameErr = /[0-9]/;
+	var emailErr = "@";
+	var webErr =/(^http:\/\/)/;
 	
-	if ($inputs.val().match(name)) {
-		console.log("noooooo");
-		$("div").children().eq(2).css("color", "red");
+	if ($name.val().match(nameErr)) {
 		$("div").children().eq(2).html("Invalid Input:  Name cannot contain numbers.");
-		// $error.css("color", "red");
 	}
-	else if($inputs.val() === "") {
+	else if($name.val() === "") {
 		$("div").children().eq(2).html("Invalid Input:  Must not be blank.");
 	}
-	// else if ($inputs.val().match(numbers))
+	else if (!$email.val().match(emailErr)) {		
+		$("div").children().eq(4).html("Invalid Input:  Email must contain @ symbol.");
+	}
+	else if ($email.val() === "") {
+		$("div").children().eq(4).html("Invalid Input:  Must not be blank.");
+	}
+	else if (!$website.val().match(webErr)) {
+		$("div").children().eq(6).html("Invalid Input:  Website must contain http://");
+	}
+	else if($website.val() === "") {
+		$("div").children().eq(6).html("Invalid Input:  Must not be blank.");
+	}
+	else if ($txtArea.val() === "") {
+		$("div").children().eq(8).html("Invalid Input:  Message must not be left blank.");
+		$("div").children().eq(8).css("marginBottom", "5px");
+	}
+	else {
+		$form.hide();
+		$hiddenDiv.show();
+		$hiddenDiv.html("Thanks for contacting us "+ $name.val()+ ".  We have received your message and will be in touch with you shortly.");
+	}
 
 }
 
@@ -43,13 +71,29 @@ $form.css("height", "500px");
 $form.css("paddingTop", "10px");
 $form.css("boxSizing", "border-box")
 
-$inputs.css("display", "block");
-$inputs.css("width", "90%");
-$inputs.css("marginLeft", "auto");
-$inputs.css("marginRight", "auto");
-$inputs.css("marginBottom", "-5px");
-$inputs.css("height", "5%");
-$inputs.css("color", "#a3a2a7");
+$name.css("display", "block");
+$name.css("width", "90%");
+$name.css("marginLeft", "auto");
+$name.css("marginRight", "auto");
+$name.css("marginBottom", "-5px");
+$name.css("height", "5%");
+$name.css("color", "#a3a2a7");
+
+$email.css("display", "block");
+$email.css("width", "90%");
+$email.css("marginLeft", "auto");
+$email.css("marginRight", "auto");
+$email.css("marginBottom", "-5px");
+$email.css("height", "5%");
+$email.css("color", "#a3a2a7");
+
+$website.css("display", "block");
+$website.css("width", "90%");
+$website.css("marginLeft", "auto");
+$website.css("marginRight", "auto");
+$website.css("marginBottom", "-5px");
+$website.css("height", "5%");
+$website.css("color", "#a3a2a7");
 
 $txtArea.css("display", "block");
 $txtArea.css("width", "90%");
@@ -68,7 +112,17 @@ $btn.css("marginLeft", "5%");
 $error.css("marginLeft", "5%");
 $error.css("marginTop", "0px");
 $error.css("marginBottom", "0px");
-$error.css("color", "white");
+$error.css("color", "red");
+
+$hiddenDiv.css("width", "80%");
+$hiddenDiv.css("marginLeft", "auto");
+$hiddenDiv.css("marginRight", "auto");
+$hiddenDiv.css("backgroundColor", "white");
+$hiddenDiv.css("textAlign", "center");
+$hiddenDiv.css("height", "150px");
+$hiddenDiv.css("marginTop", "50px");
+$hiddenDiv.css("fontSize", "20px");
+$hiddenDiv.css("paddingTop", "50px");
 
 $("#form label").css("marginLeft", "5%");
 $("#form label").css("color", "#1c1e2d");

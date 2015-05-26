a2771234fe4119573097a95a8be685aabcd9f654
Rob <rdaly1490@gmail.com>
2015-05-26 14:55:32 -0500

    update before leaving

--- dist/main.js
+++ dist/main.js
@@ -4,8 +4,10 @@ function start(eventObj) {
 
 var $form = $("#form");
 var $btn = $("#btn");
-// var $txtArea = $("#txt-area")
 $inputs = $(".text");
+
+
+
 
 $("body").css("backgroundColor", "#1c1e2d");
 $form.css("backgroundColor", "white");
@@ -23,6 +25,7 @@ $inputs.css("marginLeft", "auto");
 $inputs.css("marginRight", "auto");
 $inputs.css("marginBottom", "10px");
 $inputs.css("height", "5%");
+$inputs.css("color", "#a3a2a7");
 
 $("#text-area").css("display", "block");
 $("#text-area").css("width", "90%");
@@ -30,6 +33,7 @@ $("#text-area").css("marginLeft", "auto");
 $("#text-area").css("marginRight", "auto");
 $("#text-area").css("height", "10%");
 $("#text-area").css("marginBottom", "10px");
+$("#text-area").css("color", "#a3a2a7");
 
 $btn.css("backgroundColor", "#1c1e2d");
 $btn.css("color", "white");
@@ -38,6 +42,8 @@ $btn.css("height", "60px");
 $btn.css("marginLeft", "5%");
 
 $("#form label").css("marginLeft", "5%");
+$("#form label").css("color", "#1c1e2d");
+
 $("#form h1").css("marginLeft", "5%");
 // $("#form label").css("marginBottom", "100px");
 

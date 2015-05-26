e638e4b047cd3c27c49eccd4bf797d7a7e6b281d
Rob <rdaly1490@gmail.com>
2015-05-26 16:37:16 -0500

    surprising progress displaying errors

--- dist/main.js
+++ dist/main.js
@@ -4,7 +4,24 @@ function start(eventObj) {
 
 var $form = $("#form");
 var $btn = $("#btn");
-$inputs = $(".text");
+var $txtArea = $("#text-area");
+var $error = $(".hidden");
+var $inputs = $(".text");
+
+
+$btn.on("click", submitClicked);
+
+function submitClicked(e) {
+
+	console.log("submitClicked");
+	var re = /[0-9]/;
+	if ($inputs.val().match(re)) {
+		console.log("noooooo");
+		$error.css("color", "red");
+	}
+
+}
+
 
 
 
@@ -15,31 +32,36 @@ $form.css("width", "80%");
 $form.css("marginLeft", "auto");
 $form.css("marginRight", "auto");
 $form.css("marginTop", "50px");
-$form.css("height", "450px");
-$form.css("paddingTop", "50px");
+$form.css("height", "500px");
+$form.css("paddingTop", "10px");
 $form.css("boxSizing", "border-box")
 
 $inputs.css("display", "block");
 $inputs.css("width", "90%");
 $inputs.css("marginLeft", "auto");
 $inputs.css("marginRight", "auto");
-$inputs.css("marginBottom", "10px");
+$inputs.css("marginBottom", "-10px");
 $inputs.css("height", "5%");
 $inputs.css("color", "#a3a2a7");
 
-$("#text-area").css("display", "block");
-$("#text-area").css("width", "90%");
-$("#text-area").css("marginLeft", "auto");
-$("#text-area").css("marginRight", "auto");
-$("#text-area").css("height", "10%");
-$("#text-area").css("marginBottom", "10px");
-$("#text-area").css("color", "#a3a2a7");
+$txtArea.css("display", "block");
+$txtArea.css("width", "90%");
+$txtArea.css("marginLeft", "auto");
+$txtArea.css("marginRight", "auto");
+$txtArea.css("height", "10%");
+$txtArea.css("marginBottom", "-10px");
+$txtArea.css("color", "#a3a2a7");
 
 $btn.css("backgroundColor", "#1c1e2d");
 $btn.css("color", "white");
 $btn.css("width", "40%");
 $btn.css("height", "60px");
 $btn.css("marginLeft", "5%");
+
+$error.css("marginLeft", "5%");
+$error.css("marginTop", "0px");
+$error.css("marginBottom", "0px");
+$error.css("color", "white");
 
 $("#form label").css("marginLeft", "5%");
 $("#form label").css("color", "#1c1e2d");
